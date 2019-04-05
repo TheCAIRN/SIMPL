1954b830aecb2939ada1ca970d4bc3f4ada211c9
Tyler Carrier <32909051+tycarrier20@users.noreply.github.com>
2019-04-05 16:32:49 -0400

    Add files via upload
    Created create_variable function
    Created assign_value function
    
    created test case for variable/assign creation

--- SIMPL_Processor.py
+++ SIMPL_Processor.py
@@ -6,34 +6,56 @@ Created on Wed Mar 27 08:47:24 2019
 """
 
 class SIMPL_Processor:
+    
     def __init__(self):
         self.symbols = {}
         self.code = []
         self.current_project = ""
         self.current_line = 0
+    
     def open_project(self,name):
         # Open a project file and store the results in self.code
         self.current_project = name
+    
     def save_project(self,name):
         return
+    
     def run_project(self):
         return
+    
     def line(self,number):
         if number >= 0 and number < self.code.count:
             return self.code[number]
         else:
             return ""
+    
     def create_variable(self,name):
+        #insert string into symbols
+        str = ""
+        self.symbols[name] = str
+        
         return
+    
     def create_array(self,name):
         return
+    
     def sort(self,name):
         return
-    def assign_value(self,name):
+    
+    def assign_value(self,name,value):
+        self.symbols[name] = value
         return
+    
     def separate(self,name,value):
         return
+    
     def comment(self,comment):
         return
+    
     def join(self,name1,name2):
-        return
+        return self.symbols[name1] + " " + self.symbols[name2]
+    
+    
+    
+    
+    
