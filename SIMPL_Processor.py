ee6a28789e870a14d3df826af633ee17985a403b
The Cairn Artificial Intelligence Research Network <34629742+TheCAIRN@users.noreply.github.com>
2019-04-23 17:47:43 -0400

    Updates to processor

--- SIMPL_Processor.py
+++ SIMPL_Processor.py
@@ -4,8 +4,9 @@ Created on Wed Mar 27 08:47:24 2019
 
 @author: msabal
 """
-
 class SIMPL_Processor:
+    import TheActualOne as p
+    parser = p.Parser()
     
     def __init__(self):
         self.symbols = {}
@@ -21,13 +22,17 @@ class SIMPL_Processor:
         return
     
     def run_project(self):
-        return
+        for line in self.code:
+            parser.parse(line)
     
     def line(self,number):
         if number >= 0 and number < self.code.count:
             return self.code[number]
         else:
             return ""
+        
+    def change(self,number,command):
+        self.code[number] = command
     
     def create_variable(self,name):
         #insert string into symbols
@@ -54,6 +59,15 @@ class SIMPL_Processor:
     
     def join(self,name1,name2):
         return self.symbols[name1] + " " + self.symbols[name2]
+    
+    def say(self,target,name):
+        # If target = 0, print to console
+        # If target = 1, call the text-to-speech engine
+        if target == 0:
+            print(self.symbols[name])
+        if target == 1:
+            # To do
+            
     
     
     
