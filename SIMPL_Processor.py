b9e8b6b4bc300a0a739d495ecb731ab4526044c4
Miguel Tapia <mtapiafdez@gmail.com>
2020-07-23 13:00:35 -0400

    Cleaning up processor and working on methods

--- SIMPL_Processor.py
+++ SIMPL_Processor.py
@@ -1,103 +1,126 @@
 """
 Created on Wed Mar 27 08:47:24 2019
-@authors: msabal, tc595
+@authors: msabal, tc595, mtapia
 
 Notes:
+- created create_array function
+- created comment function
+- created sort function
+- created separate function
 
-created create_array function
-created comment function
-created sort function
-created separate function
-
+Sources:
+- https://stackoverflow.com/questions/3277503/
+how-to-read-a-file-line-by-line-into-a-list
 """
+
+import os.path
+#import TheActualParser as p
+
+
 class SIMPL_Processor:
-    import TheActualOne as p
-    parser = p.Parser()
-    
     def __init__(self):
+        # self.parser = p.Parser()
         self.symbols = {}
         self.code = []
         self.current_project = ""
         self.current_line = 0
-    
-    def open_project(self,name):
-        # Open a project file and store the results in self.code
-        self.current_project = name
-    
-    def save_project(self,name):
+
+    def open_project(self, name):
+        # Opens Project & Stores Info
+        if os.path.isfile(name):
+            with open(name) as file_in:
+                for line in file_in:
+                    self.code.append(line.rstrip())
+            self.current_project = name  # TODO: Remove .simpl if there
+        else:
+            print("Project non-existent")
+
+    def save_project(self, name):
+        # Saves Project To File
         return
-    
+
     def run_project(self):
+        # Goes Over Lines, Parser Sends Each Command Here
         for line in self.code:
-            parser.parse(line)
-    
-    def line(self,number):
+            self.parser.parse(line)
+
+    def line(self, number):
+        # Gives Line Requested By Parser Or Nothing If Non-Existent
         if number >= 0 and number < self.code.count:
             return self.code[number]
         else:
             return ""
-        
-    def change(self,number,command):
+
+    def change(self, number, command):
         self.code[number] = command
-    
-    def create_variable(self,name):
-        #insert string into symbols
+
+    def create_variable(self, name):
+        # insert string into symbols
         str = ""
         self.symbols[name] = str
-        
+
         return
-    
-    def create_array(self,name):
-        #insert array into symbols
+
+    def create_array(self, name):
+        # insert array into symbols
         arr = []
-        
-        #create array
+
+        # create array
         for name in arr:
-            symbols.append(name)
-            
+            self.symbols.append(name)
+
         return
-    
-    def sort(self,name,arr):
+
+    def sort(self, name, arr):
         length = len(arr)
-        
+
         for i in range(length):
-            for i in range(0, length - i -1):
-                if arr[j] > arr[j+1] :
+            for j in range(0, length - i - 1):
+                if arr[j] > arr[j+1]:
                     arr[j], arr[j+1] = arr[j+1], arr[j]
-                    
+
         self.symbols[name] = arr
-        
+
         return
-    
-    def assign_value(self,name,value):
+
+    def assign_value(self, name, value):
         self.symbols[name] = value
         return
-    
-    def separate(self,name,value):
+
+    def separate(self, name, value):
         str = ""
         for value in str:
             str.split(value)
-            
-        self.symbols[name] = str
-        
-        return
-    
-    def comment(self,comment):
-        #insert comment into symbols
-        comment = ""
 
-        #create comment
-        self.symbols[name] = "#" + comment
-        
+        self.symbols[name] = str
+
         return
-    
-    def join(self,name1,name2):
+
+    def comment(self, comment):
+        # insert comment into symbols
+        # comment = ""
+
+        # create comment
+        # self.symbols[name] = "#" + comment
+
+        return
+
+    def join(self, name1, name2):
         return self.symbols[name1] + " " + self.symbols[name2]
-    
-    def say(self,target,name):
+
+    def say(self, target, name):
         # If target = 0, print to console
         # If target = 1, call the text-to-speech engine
         if target == 0:
             print(self.symbols[name])
         if target == 1:
             # To do
+            return
+
+
+main_processor = SIMPL_Processor()
+
+main_processor.open_project("ProcessorTest.simpl")
+
+print(main_processor.code)
+print(main_processor.current_project)
