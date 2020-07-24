576b4d6fad8f429ba666ebc192614531f5f014a8
Miguel Tapia <mtapiafdez@gmail.com>
2020-07-24 08:11:51 -0400

    Overall structure and organize

---
+++ tests/VariableTest.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr  4 08:50:52 2019
+
+@author: tc595
+"""
+
+import SIMPL_Processor
+
+pr = SIMPL_Processor.SIMPL_Processor()
+
+pr.create_variable("hello")
+pr.create_variable("world")
+
+
+pr.assign_value("hello", "hello")
+pr.assign_value("world", "world")
+
+print(pr.join("hello", "world"))
+
+pr.run_project
