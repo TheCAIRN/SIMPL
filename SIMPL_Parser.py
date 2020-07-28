307a9365d5f946f3cbb58110b98eb52b49e70f0d
Miguel Tapia <mtapiafdez@gmail.com>
2020-07-28 18:28:40 -0400

    Moving files and cleaning up commands

--- SIMPL_Parser.py
+++ SIMPL_Parser.py
@@ -1,126 +1,140 @@
 # -*- coding: utf-8 -*-
 """
-Created on Tue Mar 26 23:24:14 2019
+Created on Sun Jul 26 20:42:02 2020
 
-@author: sw344
+@author: Daniel
 """
-import SIMPL_Processor
 
+import speech_recognition as sr
+import numpy as np
+#------------------------------------------Sample Functions-------------------------------------------------------------
+def addition(obj, parameters):
+    BUILT_IN_VARIABLES[obj] += sum(parameters)
 
-class Parser:
-        pr = SIMPL_Processor.SIMPL_Processor()
-        def Parse(input):
-            input == (input.split(" "))
-        
-    #Management Commands
-        
-    #Open Project and Open new Project
-        if input[0] == "Open":
-            if input[1] == "Project":
-                pr.open_project(input [2])
-        elif input[1] == "New":
-           pr.create_project(input [3])
-        
-    #Save Project with name
-        if input[0] == "Save":
-            if input[1] == "Project":
-                pr.save_project(input [2])
-        
-    #Run Project(must already be in file with soemthing to run, error should come from processor)
-        if input[0] == "Run":
-            pr.run_project
-        
-        
-    #Navigation Commands
-        
-    #Read line|lines command needs some more decisions before progress can continue
-        if input[0] == "Read":
-            if input[1] == "Line":
-                pr.line(input [2])
-        
-    #Reads Mulitple Lines
-        if input[1] == "Lines" and len(input) == 5:
-            if input[4] == "End":
-                pr.readEnd
-        elif input [3] == "TO":
-           pr.readLines
-                
-        
-    #NEED FUNCTION FOR STOP IN PROCESSOR   
-        #if input[0] == "Stop":
-             
-    #NEED FUNCTIONS FOR THESE COMMANDS GOT TO THE BEGINNING OR END OF A LINE  
-        if input[0] == "Go":
-            if input[3] == "Beginning":
-               pr.beginLine
-            elif input[3] == "End":
-                pr.endLine
+def multiplication(obj, parameters):
+    BUILT_IN_VARIABLES[obj] *= np.prod(parameters)
+
+def set_value(obj, value):
+    BUILT_IN_VARIABLES[obj] = value
+#------------------------------------------Definitions------------------------------------------------------------------
+class function:
+    def __init__(self, name, keyword, model, parameters, call):
+        #class to manage the parsing of specific functions
+        self.name = name
+        self.keyword = keyword
+        self.model = model
+        self.parameters = parameters
+        self.call = call
+
+#each function has specific order and keywords that make it sound natural
+add = function("add", "to", "CPKO", "inta,int", addition)
+multiply = function("multiply", "by", "COKP", "int,inta", multiplication)
+set_func = function("set", "to", "COKP", "int,int", set_value)
+
+#current list of commands that work
+BUILT_IN_COMMANDS = {"add" : add,
+                     "multiply" : multiply,
+                     "set" : set_func}
+
+x = 1
+y = 5
+BUILT_IN_VARIABLES = {"x" : x,
+                      "y" : y}
+
+#------------------------------------------Parser------------------------------------------------------------------
+def parse (input):    
+    print(input)
+    #make lowercase string
+    input = input.lower()
     
+    #split into words
+    words = input.split(" ")
     
-    #NEED FUNCTION FOR THIS? 
-        if input[0] == "Change":
-            if input[1] == "Line":
-                if input[3] == "To":
-                    parm2 = ' '+pr.join(input[4:])
-                    pr.change(input[2], parm2)
+    #get command function and remove from list
+    command = BUILT_IN_COMMANDS[words.pop(0)]
+    
+    #split between the parameters and variable
+    key_values = " ".join(words).split(command.keyword)
+    #get the parameter requirements
+    param_type = command.parameters.split(",")
+    
+    #Models:
+    #CPKO - Command Parameter Keyword Object
+    #COKP - Commands Object Keyword Parameter
+    #using the model, get the variable and the parameters
+    index = 0
+    for letter in command.model:
+        if(letter == "O"):
+            #remove spaces and get the string version of the object
+            obj = key_values[index].strip(" ")
+            index += 1
+        if(letter == "P"):
+            #remove spaces and get the parameters
+            pval = key_values[index].strip(" ")
+            #only turn into a list if there are enough elements (saves hasle later)
+            if(pval.find(" ") > 0):
+                pval = pval.split(" ")
+            parameters = check_type(pval, param_type[index])
+            index += 1
+    #call the function
+    print(parameters, obj)
+    command.call(obj, parameters)
+    print(BUILT_IN_VARIABLES[obj])
+
+#------------------------------------------Helper Function------------------------------------------------------------------
+def check_type(value, pattern):
+    try:
+        #turn the input string into the specifed type
+        if(pattern == "int"):
+            #if it's a number string, just convert it
+            if(value.isnumeric()):
+                value = int(value)
             
-        
-    #Input Commands
-    
-    #Create Variables and Create Array
-        if input[0] == "Create":
-            if input[1] == "Variable":
-                pr.create_variable(input [2])
-            elif input[1] == "Array":
-                pr.create_array(input [2])
-                if input[3] == "Includes":
-                    pr.assign_value(input[2],input[4])
-        
-    #Sort Numerically and Alphabetically
-        if input[0] == "Sort":
-            if input[2] == "Numerically":
-                pr.numSort (input[1])
-        
-        if input[0] == "Sort":
-            if input[2] == "Alphabetically":
-                pr.alphSort (input[1])
-    
-    #Set values for variables or arrays
-     #I believe this command may need to be changed in the processor from assign value to set value   
-        if input [0] == "Set":
-            pr.assign_value(input [2])
-        
-    #Separate by a certain character etc.     
-    #NEED FUNCTION FROM THE PROCESSOR FOR THIS?  
-        if input [0] == "Separate":
-            if input[3] == "By":
-                pr.separate
-        
-    #User can generate comments    
-    #NEED FUNCTION FROM THE PROCESSOR FOR THIS How do I do the END COMMENT?  
-        if input [0] == "Comment":
-            pr.comment
-        
-    #Join things together      
-        if input [0] == "Join":
-            if input.length == 6:
-                pr.join(input[1],input[3], input[5])
-            elif input.length == 4 and input[2]=="to":
-                pr.join(input[1],input[3])
+        if(pattern == "inta"):
+            if(type(value) == type([])):
+                value.remove("and")
+                out = []
+                for i in value:
+                    if(i.isnumeric()):
+                        out.append(int(i))
+                    else:
+                        out.append(BUILT_IN_VARIABLES[i])
+                value = out
             else:
-                pr.throw_error("Bad")
-        
-    #Output Commands:
-      
-    #Say and Print will call the say function just with a different parameter to differ between the two. Did I do this corectly? 
-    #Say for the program to be read back to the user   
-        if input[0] == "Say":
-            pr.say(input[1])
-    #Print for Print command to be inserted or for testing scripts?    
-        if input[0] == "Print":
-            pr.say(input[0])
-        
-            
-        
-        
-        
+                #if it's a number string, just convert it
+                if(value.isnumeric()):
+                    value = [int(value)]
+                else:
+                    #if not, its a variable, get it
+                    value = [BUILT_IN_VARIABLES[value]]
+    except:
+        print("Error: Expected %s, Recieved %s" % (pattern, type(value)))
+    return value
+
+def get_variable(name, pattern):
+    #do type checking ?
+    return BUILT_IN_VARIABLES[name.strip(" ")]
+    
+#------------------------------------------Audio------------------------------------------------------------------
+#parse("Set y to 4")
+#parse("Multiply x by y")
+#parse("Add 1 y 3 4 and x to y")
+
+import speech_recognition as sr
+
+# obtain audio from the microphone
+r = sr.Recognizer()
+with sr.Microphone() as source:
+    print("Say something!")
+    audio = r.listen(source)
+
+# recognize speech using Google Speech Recognition
+try:
+    # for testing purposes, we're just using the default API key
+    # to use another API key, use `r.recognize_google(audio, key="GOOGLE_SPEECH_RECOGNITION_API_KEY")`
+    # instead of `r.recognize_google(audio)`
+    print("Google Speech Recognition thinks you said: " + r.recognize_google(audio))
+except sr.UnknownValueError:
+    print("Google Speech Recognition could not understand audio")
+except sr.RequestError as e:
+    print("Could not request results from Google Speech Recognition service; {0}".format(e))
---
+++ docs/Commands.md
@@ -0,0 +1,50 @@
+## Project management commands:
+
+-   [x] OPEN **[NEW]** PROJECT **{Name}**
+-   [x] SAVE PROJECT **{Name}**
+-   [x] RUN PROJECT
+
+## Program navigation commands:
+
+-   [ ] READ LINE|LINES **{number}** TO **{number}** | [THE] END
+-   [ ] STOP READING
+-   [ ] [GO TO THE] **[beginning of | end of]** LINE **{number}**
+-   [ ] CHANGE LINE **{number}** TO **{command}**
+
+## Memory commands:
+
+-   [ ] CREATE VARIABLE **{name}**
+-   [ ] CREATE ARRAY **{name}** [INCLUDES **{values}**]
+-   [ ] SORT **{name}** NUMERICALLY|ALPHABETICALLY
+-   [ ] SET **{name}** [EQUAL] TO **{value}**
+-   [ ] SEPARATE **{name}** BY **{value}**
+    -   Takes a string and creates an array of strings, split by **{value}**.
+-   [ ] COMMENT ... END COMMENT
+-   [ ] JOIN **{name}** TO **{name}** [AS **{name}**]
+    -   If the third name exists, assign the result to that variable, otherwise assign it to the first.
+
+## Math commands:
+
+### Basic Math Commands
+
+-   [ ] ADD **{number}** AND **{number}**
+    -   [AND **{number}**] statement can be repeated
+-   [ ] SUBTRACT **{number}** BY **{number}**
+-   [ ] DIVIDE **{number}** BY **{number}**
+-   [ ] MULTIPLY **{number}** BY **{number}**
+-   [ ] SQUARE ROOT OF **{number}**
+-   [ ] **{number}** POWER OF **{number}**
+-   [ ] ADD **{variable}** AND **{variable}**
+    -   For string concetenation and number variables
+-   [ ] SUBTRACT **{variable}** BY **{variable}**
+
+## Output commands:
+
+-   [ ] SAY [VARIABLE] **{number}**
+    -   Use speech synthesis to dictate the value of the variable
+-   [ ] PRINT [VARIABLE] **{number}**
+    -   Display the contents of the variable on the console
+
+## Input commands:
+
+### ** TO DO **
